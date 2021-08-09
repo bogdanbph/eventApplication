9c0e2e06ce14f7c08a27bbaad835e4d09e34bb03
banton <bogdan.anton@endava.com>
2021-08-09 23:00:35 +0300

    Project commit

---
+++ src/test/java/com/erykandbogdan/eventapp/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erykandbogdan.eventapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
