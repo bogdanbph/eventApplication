589fb24c527d754364de1e269127ef7071798b80
banton <bogdan.anton@endava.com>
2021-08-12 11:17:26 +0300

    more

--- backend-app/src/main/java/com/erykandbogdan/eventapp/security/config/ApplicationSecurityConfig.java
+++ backend-app/src/main/java/com/erykandbogdan/eventapp/security/config/ApplicationSecurityConfig.java
@@ -91,22 +91,4 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setUserDetailsService(applicationUserService);
         return provider;
     }
-
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername("bogdanbph@gmail.com");
-        mailSender.setPassword("nenitescu99");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
-    }
 }
